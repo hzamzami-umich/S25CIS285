e80a245ba2fbf4787cf395def20972777b853b8f
Hebba <hzamzami@umich.edu>
2025-06-11 18:57:47 -0400

    Loop 13

--- Hello.java
+++ Hello.java
@@ -3,8 +3,8 @@
 public class Hello {
 	public static void main(String[] args) {
 
-		// For loop going from 1 to 12
-		for(int i=1;i<=12;i++) {
+		// For loop going from 1 to 13
+		for(int i=1;i<=13;i++) {
 
 			System.out.println("Hello World..."+i);
 		}
